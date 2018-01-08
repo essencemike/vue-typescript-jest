a3b97be72071cb322214d1f36af0525e92c412c0
IMike <gzxessence@163.com>
2018-01-08 10:40:36 +0800

    edit test add test to tsconfig edit HelloWorld.spec.ts

--- test/unit/specs/HelloWorld.spec.ts
+++ test/unit/specs/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld';
+import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(HelloWorld);
     const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
-    .toEqual('Welcome to Your Vue.js App');
+    const h1 = <HTMLHeadingElement>vm.$el.querySelector('.hello h1');
+    expect(h1.textContent).toEqual('Welcome to Your Vue.js App');
   });
 });
